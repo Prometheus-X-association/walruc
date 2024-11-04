219777c546f5323d368f12cbcd8388cef7e8f0d5
pierrocknroll <pierrock+github@protonmail.com>
2024-11-04 15:28:03 +0100

    Exponential backoff for HTTP requests

--- LearningRecordConverter/ConverterApi.php
+++ LearningRecordConverter/ConverterApi.php
@@ -9,10 +9,12 @@ use Piwik\Http;
 use Piwik\Log\LoggerInterface;
 use Piwik\Plugins\Walruc\Exceptions\ConversionException;
 use Piwik\Plugins\Walruc\Tracker\TrackingData;
-use Throwable;
+use Piwik\Plugins\Walruc\Traits\RetryableHttpTrait;
 
 class ConverterApi implements ConverterInterface
 {
+    use RetryableHttpTrait;
+
     private const INPUT_FORMAT = 'matomo';
     private const TIMEOUT_SECONDS = 10;
     private const ENDPOINT_CONFIG = 'lrcEndpoint';
@@ -35,30 +37,30 @@ class ConverterApi implements ConverterInterface
 
         $this->logger->info('Converting data', ['body' => $body]);
 
-        try {
-            $jsonResponse = Http::sendHttpRequestBy(
-                method: Http::getTransportMethod(),
-                aUrl: $this->config->getFromLocalConfig('Walruc')[self::ENDPOINT_CONFIG],
-                timeout: self::TIMEOUT_SECONDS,
-                httpMethod: 'POST',
-                requestBody: json_encode($body, JSON_INVALID_UTF8_IGNORE),
-                additionalHeaders: [
-                    'Accept: application/json',
-                    'Content-Type: application/json; charset=utf-8',
-                ],
-            );
+        return $this->executeWithRetry(
+            operation: function () use ($body) {
+                $jsonResponse = Http::sendHttpRequestBy(
+                    method: Http::getTransportMethod(),
+                    aUrl: $this->config->getFromLocalConfig('Walruc')[self::ENDPOINT_CONFIG],
+                    timeout: self::TIMEOUT_SECONDS,
+                    httpMethod: 'POST',
+                    requestBody: json_encode($body, JSON_INVALID_UTF8_IGNORE),
+                    additionalHeaders: [
+                        'Accept: application/json',
+                        'Content-Type: application/json; charset=utf-8',
+                    ],
+                );
 
-            if (!$jsonResponse) {
-                throw ConversionException::conversionFailed('Conversion API returned an error');
-            }
+                if (!$jsonResponse) {
+                    throw ConversionException::conversionFailed('Conversion API returned an error');
+                }
 
-            $this->logger->info('Convert response received from LRC', ['response' => $jsonResponse]);
+                $this->logger->info('Convert response received from LRC', ['response' => $jsonResponse]);
 
-            return ConverterResponse::fromJson($jsonResponse);
-        } catch (Throwable $e) {
-            throw $e instanceof ConversionException
-                ? $e
-                : ConversionException::conversionFailed($e->getMessage(), $e);
-        }
+                return ConverterResponse::fromJson($jsonResponse);
+            },
+            logger: $this->logger,
+            operationName: 'Converting data',
+        );
     }
 }
